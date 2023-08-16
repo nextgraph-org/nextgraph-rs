4ea9be803a86dcf2861c65b5ed1bd919dd74f3ef
Niko PLP <niko@nextgraph.org>
2023-08-16 08:54:38 +0300

    new version of rocksdb + fs_Sync

--- stores-rocksdb/src/kcv_store.rs
+++ stores-rocksdb/src/kcv_store.rs
@@ -406,6 +406,7 @@ impl RocksdbKCVStore {
     /// The key is the encryption key for the data at rest.
     pub fn open<'a>(path: &Path, key: [u8; 32]) -> Result<RocksdbKCVStore, StorageError> {
         let mut opts = Options::default();
+        opts.set_use_fsync(true);
         opts.create_if_missing(true);
         opts.create_missing_column_families(true);
         let env = Env::enc_env(key).unwrap();
