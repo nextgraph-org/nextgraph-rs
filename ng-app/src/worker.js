e652824fe3aac873faacf8b6c0ad486d69e5bca9
Niko PLP <niko@nextgraph.org>
2023-09-07 23:57:02 +0300

    fix worker race condition issue

--- ng-app/src/worker.js
+++ ng-app/src/worker.js
@@ -1,21 +1,24 @@
-import {
-    default as ng,
-  } from "./api";
+import * as api from "ng-sdk-js";
+import { default as ng } from "./api";
 
-  onmessage = (e) => {
-    console.log("Message received by worker", e.data);
-    (async function() {
-      try {
-        let secret_wallet = await ng.wallet_open_wallet_with_pazzle(
-            e.data.wallet,
-            e.data.pazzle,
-            e.data.pin_code
-        );
-        postMessage({success:secret_wallet});
-      } catch (e) {
-        postMessage({error:e});
-      }
-    })();
-  };
+//console.log("loaded worker");
+
+onmessage = (e) => {
+  //console.log("Message received by worker", e.data);
+  (async function() {
+    try {
+      let secret_wallet = await ng.wallet_open_wallet_with_pazzle(
+          e.data.wallet,
+          e.data.pazzle,
+          e.data.pin_code
+      );
+      postMessage({success:secret_wallet});
+    } catch (e) {
+      postMessage({error:e});
+    }
+  })();
+};
+
+postMessage({loaded:true});
 
   
