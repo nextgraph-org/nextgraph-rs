75470399984ccd7b2cb6b3f049b93bc2805ac252
Laurin Weger <Laurin-W@users.noreply.github.com>
2024-07-02 16:02:35 +0200

    fix merge-conflict-broken loading spinner

--- ng-app/src/lib/Login.svelte
+++ ng-app/src/lib/Login.svelte
@@ -381,8 +381,16 @@
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             stroke="currentColor"
-            stroke-width="4"
+            viewBox="0 0 24 24"
           >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            />
             <path
               class="opacity-75"
               fill="currentColor"
