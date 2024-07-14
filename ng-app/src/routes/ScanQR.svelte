700413d31ab500d9995b968c2642548c3b01ba0c
Niko PLP <niko@nextgraph.org>
2024-07-14 04:18:56 +0300

    try again if no rear camera

--- ng-app/src/routes/ScanQR.svelte
+++ ng-app/src/routes/ScanQR.svelte
@@ -56,12 +56,21 @@
       //   { fps: 10, qrbox: { width: 300, height: 300 }, formatsToSupport: [0] },
       //   false
       // );
-      webScanner = new Html5Qrcode ("scanner-div");
-      webScanner.start({ facingMode: { exact: "environment"} }, { fps: 10, qrbox: { width: 300, height: 300 }, formatsToSupport: [0] }, (decoded_text, decoded_result) => {
-        //console.log(decoded_result);
-        // Handle scan result
-        on_qr_scanned(decoded_text);
-      });
+      try {
+        webScanner = new Html5Qrcode ("scanner-div");
+        webScanner.start({ facingMode: { exact: "environment"} }, { fps: 10, qrbox: { width: 300, height: 300 }, formatsToSupport: [0] }, (decoded_text, decoded_result) => {
+          //console.log(decoded_result);
+          // Handle scan result
+          on_qr_scanned(decoded_text);
+        });
+      } catch (e) {
+        webScanner = new Html5Qrcode ("scanner-div");
+        webScanner.start({ facingMode: "environment" }, { fps: 10, qrbox: { width: 300, height: 300 }, formatsToSupport: [0] }, (decoded_text, decoded_result) => {
+          //console.log(decoded_result);
+          // Handle scan result
+          on_qr_scanned(decoded_text);
+        });
+      }
 
       // // Add scanner to Screen.
       // webScanner.render((decoded_text, decoded_result) => {
