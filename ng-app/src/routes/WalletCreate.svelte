c2b75e9df15a87cc66c255a4ea61933db53a6268
Niko PLP <niko@nextgraph.org>
2024-08-13 21:04:02 +0300

    fix style on WalletCreate

--- ng-app/src/routes/WalletCreate.svelte
+++ ng-app/src/routes/WalletCreate.svelte
@@ -984,7 +984,7 @@
                     d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
                   />
                 </svg>
-                {$t("pages.wallet_create.register_with", {
+                {$t("pages.wallet_create.register_with_broker", {
                   values: { broker: pre_invitation.V0.name || "this broker" },
                 })}
               </button>
@@ -1108,7 +1108,8 @@
             </button>
           </div> -->
           {#if import.meta.env.NG_APP_WEB}
-          <div class="row mt-5 max-w-xl md:mx-auto lg:max-w-2xl px-4 lg:px-8 pb-0 text-left text-gray-500 dark:text-gray-400 flex space-x-3">
+
+          <div class="flex space-x-3 px-4 pt-3 lg:px-8 lg:pt-10 max-w-xl text-left text-gray-500 dark:text-gray-400">
             <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"  fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"></path>
             </svg>
