e3faf39adedd2936a795b651783ee0ab82a5dda7
Niko PLP <niko@nextgraph.org>
2025-05-01 17:44:40 +0300

    ng-broker build

--- ng-broker/src/server_ws.rs
+++ ng-broker/src/server_ws.rs
@@ -192,29 +192,29 @@ fn prepare_urls_from_private_addrs(addrs: &Vec<BindAddress>, port: u16) -> Vec<S
     res
 }
 
-#[cfg(not(docsrs))]
-#[derive(RustEmbed)]
-#[folder = "../ng-app/dist-file/"]
-#[include = "*.sha256"]
-#[include = "*.gzip"]
-struct App;
+// #[cfg(not(docsrs))]
+// #[derive(RustEmbed)]
+// #[folder = "../ng-app/dist-file/"]
+// #[include = "*.sha256"]
+// #[include = "*.gzip"]
+// struct App;
+
+// #[cfg(not(docsrs))]
+// #[derive(RustEmbed)]
+// #[folder = "../helpers/app-auth/dist/"]
+// #[include = "*.sha256"]
+// #[include = "*.gzip"]
+
+// struct AppAuth;
 
 #[cfg(not(docsrs))]
-#[derive(RustEmbed)]
-#[folder = "../helpers/app-auth/dist/"]
-#[include = "*.sha256"]
-#[include = "*.gzip"]
-
-struct AppAuth;
-
-#[cfg(docsrs)]
 #[derive(RustEmbed)]
 #[folder = "./static/app/"]
 #[include = "*.sha256"]
 #[include = "*.gzip"]
 struct App;
 
-#[cfg(docsrs)]
+#[cfg(not(docsrs))]
 #[derive(RustEmbed)]
 #[folder = "./static/app-auth/"]
 #[include = "*.sha256"]
