29a35f835a93ea3580747e954699cb87986cff0b
Niko PLP <niko@nextgraph.org>
2025-05-21 13:24:54 +0300

    cleanup and bump ldo version

--- ng-sdk-js/example-webapp-react-socialquery/src/Contact.tsx
+++ ng-sdk-js/example-webapp-react-socialquery/src/Contact.tsx
@@ -1,13 +1,13 @@
 import { default as React, FunctionComponent } from "react";
 import { useNextGraphAuth } from "./reactMethods";
 import { SocialContactShapeType } from "./.ldo/contact.shapeTypes.ts";
-import { useSubscribeToResource, useResource, useSubject } from "./reactMethods.ts";
+import { useSubscribeToResource, useResource, useSubject, useLdo } from "./reactMethods.ts";
 import { StarIcon  } from '@heroicons/react/24/solid'
 import { StarIcon as StarIconOutline, NoSymbolIcon } from '@heroicons/react/24/outline'
 
 export const Contact: FunctionComponent = ({nuri}) => {
   const { session } = useNextGraphAuth();
-
+  const { createData, commitData, changeData } = useLdo();
   useResource(session.sessionId && nuri ? nuri : undefined, { subscribe: true });
   let contact = useSubject(SocialContactShapeType, session.sessionId && nuri ? nuri.substring(0,53) : undefined);
 
@@ -37,47 +37,88 @@ export const Contact: FunctionComponent = ({nuri}) => {
     "Automerge",
   ]
 
-  const [skills, setSkills] = React.useState([
-    0,
-    0,
-    5,
-    3,
-    1,
-    2,
-    4,
-    0,
-    0,
-  ])
+  const [skills, setSkills] = React.useState([ 0, 0, 0, 0, 0, 0, 0, 0, 0 ]);
 
   React.useEffect(() => {
-    console.log(contact.hasRating?.entries())
+    //contact.hasRating?.map((r)=> {if (r.skill) console.log(r.rated,r.skill["@id"].substring(24))});
     let nextSkills = skills.map((s) => {
       return 0;
     });
     contact.hasRating?.map((r) => {
-      nextSkills[ksp_mapping.indexOf(r.skill["@id"].substring(28))] = r.rated +1;
+      nextSkills[ksp_mapping.indexOf(r.skill["@id"].substring(24))] = r.rated +1;
     });
     setSkills(nextSkills);
   }, [contact])
 
   if (!session.sessionId || !nuri) return <></>;
   
-  function rate(skill: number, rating: number) {
-    console.log("rate", skill, rating);
-    
-      const nextSkills = skills.map((s, i) => {
-        if (i === skill) {
-          if (s == rating) {
-            s = s - 1;
-          } else {
-            s = rating;
-          }
-          return s;
-        } else {
-          return s;
+  async function rate(skill: number, rating: number) {
+    const nextSkills = [...skills];
+    const old = skills[skill];
+
+    if (old == 0) {
+      if (rating == 0) return;//should happen
+      // we create a new rating
+      nextSkills[skill] = rating;
+      try {
+        await session.ng.sparql_update(session.sessionId, `PREFIX xskills: <did:ng:x:skills#>
+          PREFIX ksp: <did:ng:k:skills:programming:>
+          PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
+          INSERT { 
+            <> xskills:hasRating [
+              a xskills:Rating ;
+              xskills:rated "${rating-1}"^^xsd:integer ;
+              xskills:skill ksp:${ksp_mapping[skill]}
+            ].
+          } WHERE {}`, nuri);
+      } catch (e) {
+        console.error(e)
+      }
+    } else {
+      if (old == rating) {
+        nextSkills[skill] = rating - 1;
+      } else {
+        nextSkills[skill] = rating;
+      }
+      if (nextSkills[skill] == 0) {
+        // we remove the rating
+        try { const s = `PREFIX xskills: <did:ng:x:skills#>
+            PREFIX ksp: <did:ng:k:skills:programming:>
+            PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
+            DELETE { 
+              <> xskills:hasRating ?rating.
+              ?rating a xskills:Rating .
+              ?rating xskills:skill ksp:${ksp_mapping[skill]} .
+              ?rating xskills:rated "${old-1}"^^xsd:integer.
+            } WHERE { <> xskills:hasRating ?rating .
+                      ?rating a xskills:Rating .
+                      ?rating xskills:skill ksp:${ksp_mapping[skill]}
+            }`;
+          await session.ng.sparql_update(session.sessionId, s, nuri);
+        } catch (e) {
+          console.error(e)
         }
-      });
-      setSkills(nextSkills);
+      } else {
+        // we update the rating
+        try {
+          await session.ng.sparql_update(session.sessionId, `PREFIX xskills: <did:ng:x:skills#>
+            PREFIX ksp: <did:ng:k:skills:programming:>
+            PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
+            DELETE { 
+              ?rating xskills:rated "${old-1}"^^xsd:integer.
+            } INSERT {
+              ?rating xskills:rated "${nextSkills[skill]-1}"^^xsd:integer.
+            }
+              WHERE { <> xskills:hasRating ?rating .
+                      ?rating a xskills:Rating .
+                      ?rating xskills:skill ksp:${ksp_mapping[skill]}
+            }`, nuri);
+        } catch (e) {
+          console.error(e)
+        }
+      }
+    }
+    setSkills(nextSkills);
   }
 
   return <>
@@ -89,14 +130,12 @@ export const Contact: FunctionComponent = ({nuri}) => {
         <div className="p-2">
           <span className="email">
             {contact.hasEmail}
-            {JSON.stringify(contact.hasRating?.entries())}
           </span>
         </div>
         {
           skills.map(
             (skill,s) => 
               <div key={s} className="px-2 flex flex-row cursor-pointer text-yellow-500">
-                {/* <NoSymbolIcon className="size-6"/> */}
                 {
                   [...Array(5)].map(
                     (e,i) => {
