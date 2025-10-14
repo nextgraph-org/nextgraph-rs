0ed801ac55a00ba641e0bd804afc299ed8c0423a
Niko PLP <niko@nextgraph.org>
2025-10-14 20:23:27 +0300

    adding svelte

--- app/nextgraph/vite.config.ts
+++ app/nextgraph/vite.config.ts
@@ -1,11 +1,12 @@
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite";
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
-export default defineConfig(async () => ({
-
+export default defineConfig({
+  plugins: [tailwindcss(), svelte()],
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent Vite from obscuring rust errors
@@ -40,4 +41,4 @@ export default defineConfig(async () => ({
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
-}));
+});
