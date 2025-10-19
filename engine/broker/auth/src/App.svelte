ad94c01c699955dc34b2d65f83dfe3fe6127e01b
Niko PLP <niko@nextgraph.org>
2025-10-20 02:14:31 +0300

    fix

--- engine/broker/auth/src/App.svelte
+++ engine/broker/auth/src/App.svelte
@@ -272,11 +272,11 @@
 
           const init = () => {
             if (net_auth_iframe && port)
-            iframe.postMessage({ method: "init", session:value, manifest:{origin:origin_url}, port: port }, iframe_config.origin, [port]);
+            net_auth_iframe.postMessage({ method: "init", session:value, manifest:{origin:origin_url}, port: port }, iframe_config.origin, [port]);
           };
 
           iframe.addEventListener("load",  function() {
-            net_auth_iframe = true;//this.contentWindow;
+            net_auth_iframe = this.contentWindow;
             //console.log(net_auth_iframe);
             init();
           });
