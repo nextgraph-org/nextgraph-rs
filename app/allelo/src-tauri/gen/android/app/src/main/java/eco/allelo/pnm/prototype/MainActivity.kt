e61d97930a05963903e61fcd54a93e585d3c2979
Niko PLP <niko@nextgraph.org>
2025-10-22 16:17:44 +0300

    Tauri+android project for Allelo

---
+++ app/allelo/src-tauri/gen/android/app/src/main/java/eco/allelo/pnm/prototype/MainActivity.kt
@@ -0,0 +1,11 @@
+package eco.allelo.pnm.prototype
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
