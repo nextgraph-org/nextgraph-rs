f8cd1a59b04057074983a1d2f39a9e73f6c018ce
Niko PLP <niko@nextgraph.org>
2025-10-29 13:13:17 +0200

    fix security_img

--- app/allelo/src/svelte/WalletCreate.svelte
+++ app/allelo/src/svelte/WalletCreate.svelte
@@ -176,6 +176,7 @@
     let params = {
       pazzle_length: 0,
       security_txt: username,
+      security_img: undefined,
       password: password,
       mnemonic: false,
       send_bootstrap: false, //options.cloud || options.bootstrap ?  : undefined,
