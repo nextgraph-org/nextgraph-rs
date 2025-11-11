947cc4c6591736c348b3f60464f6d36634504631
Niko PLP <niko@nextgraph.org>
2025-11-11 18:55:49 +0200

    remove old ORM shapes

--- app/allelo/src/.shapes/orm/socialquery.shapeTypes.ts
+++
@@ -1,9 +0,0 @@
-import type { ShapeType } from "@ng-org/shex-orm";
-import { socialquerySchema } from "./socialquery.schema";
-import type { SocialQuery } from "./socialquery.typings";
-
-// ShapeTypes for socialquery
-export const SocialQueryShapeType: ShapeType<SocialQuery> = {
-  schema: socialquerySchema,
-  shape: "did:ng:x:shape#SocialQuery",
-};
