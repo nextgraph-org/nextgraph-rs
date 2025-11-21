a419801c75f7637049f0d31012faaf1c5e3d7b63
Niko PLP <niko@nextgraph.org>
2025-11-21 18:13:53 +0200

    log

--- sdk/js/web/src/index.ts
+++ sdk/js/web/src/index.ts
@@ -38,6 +38,7 @@ export const init = async function(callback:Function | null, singleton:boolean, 
   }
 }
 
+//TODO: add all the streamed functions
 const streamed_api: Record<string,number> = {
   "doc_subscribe": 2,
   "orm_start": 3,
@@ -47,9 +48,9 @@ const streamed_api: Record<string,number> = {
 
 function rpc( method:string, args?: any) : Promise<any> {
   const { port1, port2 } = new MessageChannel();
-  //console.log("POSTING",method, args);
+  console.log("POSTING",method, args);
   let callback_idx = streamed_api[method];
-  if (callback_idx !== undefined) { //TODO: add all the streamed functions
+  if (callback_idx !== undefined) { 
     let callback = args[callback_idx];
     let new_args = args.slice(0, -1);
     parent.postMessage({ method, streamed:true, args:new_args, port: port2 }, config.origin, [port2]);
