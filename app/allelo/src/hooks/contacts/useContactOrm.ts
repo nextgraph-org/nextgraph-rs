a6cc59fbdd5f2fbeab45ebb0d6cd71d319aa28b4
mkslanc <mkslanc@gmail.com>
2025-12-23 18:33:19 +0400

    type cast for orm

--- app/allelo/src/hooks/contacts/useContactOrm.ts
+++ app/allelo/src/hooks/contacts/useContactOrm.ts
@@ -2,8 +2,9 @@ import {useNextGraphAuth} from "@/lib/nextgraph";
 import {NextGraphAuth} from "@/types/nextgraph";
 import {SocialContactShapeType} from "@/.orm/shapes/contact.shapeTypes";
 import {useShape} from "@ng-org/orm/react";
+import {SocialContact} from "@/.orm/shapes/contact.typings.ts";
 
-export const useContactOrm = (nuri: string | null | undefined, isProfile = false) => {
+export const useContactOrm = (nuri: string | null | undefined, isProfile = false): { ormContact: SocialContact } => {
   const nextGraphAuth = useNextGraphAuth() || {} as NextGraphAuth;
   const {session} = nextGraphAuth;
 
@@ -13,7 +14,7 @@ export const useContactOrm = (nuri: string | null | undefined, isProfile = false
 
   const ormContacts = useShape(SocialContactShapeType, nuri ? nuri : undefined);
   const objects = [...(ormContacts || [])];
-  const ormContact = objects[0];
+  const ormContact = objects[0] as SocialContact;
 
   return {ormContact};
 };
