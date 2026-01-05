fffd441c3f532cf57efa881ae2c366ef083e816c
Niko PLP <niko@nextgraph.org>
2026-01-05 17:14:38 +0200

    test-react-sdk

---
+++ sdk/js/examples/test-react-sdk/src/App.tsx
@@ -0,0 +1,12 @@
+
+import { ReactExpenseTracker } from "./react/Main";
+import './styles/expense-tracker.css'
+
+function App() {
+
+  return (
+    <ReactExpenseTracker />
+  )
+}
+
+export default App
