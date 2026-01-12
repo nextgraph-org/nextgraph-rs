3f5c82c94643b101120db1ad1122196a1d024059
Niko PLP <niko@nextgraph.org>
2026-01-12 19:41:29 +0200

    fix reverted patch for type (extra not taken into account)

--- engine/net/src/orm.rs
+++ engine/net/src/orm.rs
@@ -95,7 +95,7 @@ pub struct OrmSchemaPredicate {
 }
 impl OrmSchemaPredicate {
     pub fn is_multi(&self) -> bool {
-        self.maxCardinality > 1 || self.maxCardinality == -1
+        self.maxCardinality > 1 || self.maxCardinality == -1 || self.extra.unwrap_or(false)
     }
     pub fn is_object(&self) -> bool {
         self.dataTypes
