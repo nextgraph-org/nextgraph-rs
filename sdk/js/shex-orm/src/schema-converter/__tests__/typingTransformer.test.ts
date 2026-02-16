926abd59cbfbd4679cf1985891b09c24bec57fa0
Laurin Weger <Laurin-W@users.noreply.github.com>
2026-02-16 19:44:18 +0100

    chore: shex-orm fix predicate collision handling

--- sdk/js/shex-orm/src/schema-converter/__tests__/typingTransformer.test.ts
+++ sdk/js/shex-orm/src/schema-converter/__tests__/typingTransformer.test.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect } from "vitest";
 import annotateReadablePredicates from "../util/annotateReadablePredicates.ts";
 import { shexJConverter } from "../converter.ts";
+import { Schema } from "../../ShexJTypes.ts";
 
 const TYPE_IRI = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
 
@@ -57,8 +58,75 @@ function buildSchema() {
                     },
                 },
             },
+            {
+                type: "ShapeDecl",
+                id: "http://example.org/CollisionTest",
+                shapeExpr: {
+                    type: "Shape",
+                    extra: [TYPE_IRI],
+                    expression: {
+                        type: "EachOf",
+                        expressions: [
+                            {
+                                type: "TripleConstraint",
+                                predicate: "http://example.org/collide1/foo",
+                                valueExpr: {
+                                    type: "NodeConstraint",
+                                    datatype:
+                                        "http://www.w3.org/2001/XMLSchema#string",
+                                },
+                            },
+                            {
+                                type: "TripleConstraint",
+                                predicate: "http://example.org/collide2/foo",
+                                valueExpr: {
+                                    type: "NodeConstraint",
+                                    datatype:
+                                        "http://www.w3.org/2001/XMLSchema#string",
+                                },
+                            },
+                            {
+                                type: "TripleConstraint",
+                                predicate: "http://example.org/collide3/foo",
+                                valueExpr: {
+                                    type: "NodeConstraint",
+                                    datatype:
+                                        "http://www.w3.org/2001/XMLSchema#string",
+                                },
+                            },
+                            {
+                                type: "TripleConstraint",
+                                predicate: "http://example.org2/collide3/foo",
+                                valueExpr: {
+                                    type: "NodeConstraint",
+                                    datatype:
+                                        "http://www.w3.org/2001/XMLSchema#string",
+                                },
+                            },
+                            {
+                                type: "TripleConstraint",
+                                predicate: "http://example.org:collide4/foo",
+                                valueExpr: {
+                                    type: "NodeConstraint",
+                                    datatype:
+                                        "http://www.w3.org/2001/XMLSchema#string",
+                                },
+                            },
+                            {
+                                type: "TripleConstraint",
+                                predicate: "http://example.org/collide4/foo",
+                                valueExpr: {
+                                    type: "NodeConstraint",
+                                    datatype:
+                                        "http://www.w3.org/2001/XMLSchema#string",
+                                },
+                            },
+                        ],
+                    },
+                },
+            },
         ],
-    };
+    }; // satisfies Schema;
 }
 
 async function buildTypingsText(): Promise<string> {
@@ -79,7 +147,18 @@ describe("ShexJTypingTransformer", () => {
     it("treats EXTRA rdf:type predicates as plural", async () => {
         const typings = await buildTypingsText();
         expect(typings).toMatch(
-            /interface ExpenseCategory[\s\S]*?"@type": Set<"http:\/\/example\.org\/ExpenseCategory" \|[\s]*\(?string[\s]*&[\s]*\{[\s]*\}\)?>;/
+            /\"@type\": Set<\"http:\/\/example\.org\/ExpenseCategory\" | IRI & {}>;/
         );
+    });
+
+    it("handles property name collisions", async () => {
+        const typings = await buildTypingsText();
+
+        expect(typings).toMatch(/ collide1_foo: string/);
+        expect(typings).toMatch(/ collide2_foo: string/);
+        expect(typings).toMatch(/ org_collide3_foo: string/);
+        expect(typings).toMatch(/ org2_collide3_foo: string/);
+        expect(typings).toMatch(/ "0__http_example_org_collide4_foo"\: string/);
+        expect(typings).toMatch(/ "1__http_example_org_collide4_foo"\: string/);
     });
 });
