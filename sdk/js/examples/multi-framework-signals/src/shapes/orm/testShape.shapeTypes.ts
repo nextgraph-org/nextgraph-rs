c41219b3d1f85fe9985f18b460c49dd60702a5c3
Laurin Weger <git@laurinweger.de>
2026-02-20 18:47:20 +0100

    rm obsolete multi-framework-signals package

--- sdk/js/examples/multi-framework-signals/src/shapes/orm/testShape.shapeTypes.ts
+++
@@ -1,9 +0,0 @@
-import type { ShapeType } from "@ng-org/shex-orm";
-import { testShapeSchema } from "./testShape.schema";
-import type { TestObject } from "./testShape.typings";
-
-// ShapeTypes for testShape
-export const TestObjectShapeType: ShapeType<TestObject> = {
-  schema: testShapeSchema,
-  shape: "http://example.org/TestObjectShape",
-};
