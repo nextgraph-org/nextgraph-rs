45f3776e7d12d336217c2f41be217793a9625e91
Niko PLP <niko@nextgraph.org>
2026-08-04 23:05:33 +0300

    ORM: insertObject returns the inserted object

--- sdk/js/orm/src/connector/insertObject.ts
+++ sdk/js/orm/src/connector/insertObject.ts
@@ -10,18 +10,23 @@
 
 import { BaseType, ShapeType } from "@ng-org/shex-orm";
 import { RdfOrmSubscription } from "./RdfOrmSubscription.ts";
+import { RAW_KEY } from "@ng-org/alien-deepsignals";
 
 /**
  * Utility for adding ORM-typed objects to the database without
  * the need for subscribing to documents using an {@link RdfOrmSubscription}.
  *
  * @param shapeType The shape type of the objects to be inserted.
- * @param object The object to be inserted.
+ * @param object The object to be inserted. The `@graph`must be set.
+ *     It is recommended to set `@id` to `""` or leave it `undefined`, to auto-generate a unique NURI.
+ *
+ * @returns the inserted object. If `@id` was set to `""` or `undefined`, it's now set to an auto-generated NURI.
+ *     This is true for nested objects as well. For nested objects, the `@graph`, if unset, will be set to the parent's `@graph`.
  */
 export async function insertObject<T extends BaseType>(
     shapeType: ShapeType<T>,
     object: T
-) {
+): Promise<T> {
     const connection = RdfOrmSubscription.getOrCreate(shapeType, {
         graphs: [], // Subscribe to no documents
     });
@@ -30,4 +35,6 @@ export async function insertObject<T extends BaseType>(
     connection.signalObject.add(object as Exclude<T, undefined>);
 
     connection.close();
+
+    return connection.signalObject.first()![RAW_KEY];
 }
