e32e3d820c0299f266372aea3f109fbfac21de13
Laurin Weger <git@laurinweger.de>
2026-09-22 15:02:41 +0200

    rdf and discrete example apps

--- sdk/js/examples/expense-tracker-discrete/astro.config.ts
+++ sdk/js/examples/expense-tracker-discrete/astro.config.ts
@@ -3,10 +3,25 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import svelte from "@astrojs/svelte";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), vue(), svelte()],
+    integrations: [
+        react({
+            include: ["**/react/**", "*/ReactRoot.tsx"],
+        }),
+        vue(),
+        svelte(),
+        solidJs({
+            include: [
+                "**/solid-js/**",
+                "**/node_modules/@suid/material/**",
+                "**/SolidJsRoot.tsx",
+            ],
+            devtools: true,
+        }),
+    ],
     srcDir: "./src/app-wrapper",
     vite: {
         server: {
